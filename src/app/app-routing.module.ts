037b56195facd560ec712bf9b1138fa3fb6b5cd8
Luca Albrecht <luca@albright.one>
2023-06-15 19:29:41 +0200

    added pages, removed boilerplate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputComponent } from './pages/input/input.component';
+import { ListComponent } from './pages/list/list.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InputComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', component: InputComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './pages/input/input.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { ListComponent } from './pages/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    NotfoundComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
